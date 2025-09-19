a6246bcdf9ba55c26706496be985f068269939dc
Sofia Johansson <sj224ds@student.lnu.se>
2025-09-19 10:53:45 +0200

    Add function for adding number of pages

--- src/main.js
+++ src/main.js
@@ -1,5 +1,17 @@
 import { Book } from './book.js'
 
-function addNumberOfPages () {
-    
+let books = [
+  new Book('The Hobbit', 'fantasy', 300),
+  new Book('Emma', 'fiction', 500)
+]
+
+function addNumberOfPages (books) {
+  let sum = 0
+  for (const book of books) {
+    sum += book.numberOfPages
+  }
+  console.log(sum)
+  return sum
 }
+
+addNumberOfPages(books)
